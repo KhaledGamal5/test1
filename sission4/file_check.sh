7eaf3c263da3a3abbcbdc3e2e0d3feb206034d77
Khaled Gamal <k.gamal.abdulzaher1@gmail.com>
2023-08-24 01:04:46 +0300

    Add files via upload

---
+++ sission4/file_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filename="$1"
+directory="$2"
+
+
+if [ -e "$directory/$filename" ]; then
+     echo "File exists"
+     echo "Contents of $filename:"
+     cat "$directory/$filename"
+else
+     echo "File does nor exist"
+fi          
